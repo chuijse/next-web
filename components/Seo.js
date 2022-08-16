206d2cca66cb7dbb8de31e88690d1e02fe7a6f90
chuijse <cristian.huijse@gmail.com>
2022-08-16 11:34:42 -0400

    image to metadata

--- components/Seo.js
+++ components/Seo.js
@@ -3,11 +3,13 @@ import Head from "next/head";
 export default function Seo({
   pageTitle = "Portfalio",
   description = "This is portfolio of Cristian Huijse Industrial Designer.",
-  image = "/favicon.ico",
+  icon = "/favicon.ico",
+  image = "/meta-data.jpeg",
   year = "2022",
   author = "Cristian Huijse Heise",
   linkedin = "https://cl.linkedin.com/in/cristianhuijse",
   github = "https://github.com/chuijse",
+  article = false,
 }) {
   return (
     <Head>
@@ -20,14 +22,19 @@ export default function Seo({
       <meta name="github" content={github} />
 
       <meta property="og:title" content={`CHH | ${pageTitle}`} key="title" />
+      <meta property="og:type" content={article ? "article" : "webpage"} />
+      <meta property="og:url" content=" http://chh.work/" />
+      <meta property="og:image" content={image} />
       <meta property="og:description" content={description} key="description" />
-      <meta name="og:author" content={author} />
-      <meta name="og:image" content={image} />
+      <meta property="og:site_name" content="Cristian Huijse Portfolio" />
+      <meta property="og:author" content={author} />
 
       <meta property="twitter:title" content={`CHH | ${pageTitle}`} />
       <meta property="twitter:description" content={description} />
-      <meta name="twitter:author" content={author} />
-      <link rel="icon" href={image} />
+      <meta property="twitter:author" content={author} />
+      <meta property="twitter:image:src" content={image} />
+
+      <link rel="icon" href={icon} />
     </Head>
   );
 }
