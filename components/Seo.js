c71064eb4322a8af6d2da8f61f2dbb6b3e080b78
chuijse <cristian.huijse@gmail.com>
2022-08-16 12:43:45 -0400

    url to seo

--- components/Seo.js
+++ components/Seo.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Seo({
-  pageTitle = "Portfalio",
+  pageTitle = "Portafolio",
   description = "Este es el portafolio de Cristian Huijse Heise, Diseñador industrial especialista en el ambito de la interacción digital",
   icon = "/favicon.ico",
   image = "/meta-data.jpg",
@@ -11,6 +12,8 @@ export default function Seo({
   github = "https://github.com/chuijse",
   article = false,
 }) {
+  const router = useRouter();
+  console.log(router);
   return (
     <Head>
       <title>{`CHH | ${pageTitle}`}</title>
@@ -27,23 +30,25 @@ export default function Seo({
       />
       <meta name="Revisit-after" content="1 days" />
       <meta name="robots" content="index, follow" />
-
+      {/* Facebook Meta Tags */}
       <meta property="og:title" content={`CHH | ${pageTitle}`} key="title" />
       <meta property="og:type" content={article ? "article" : "webpage"} />
-      <meta property="og:url" content=" http://chh.work/" />
+      <meta property="og:url" content={router.route} />
       <meta property="og:image" content={image} />
       <meta property="og:description" content={description} key="description" />
       <meta property="og:site_name" content="Cristian Huijse Portfolio" />
       <meta property="og:author" content={author} />
-
+      {/* Twitter Meta Tags */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:title" content={`CHH | ${pageTitle}`} />
+      <meta property="twitter:url" content={router.route} />
+      <meta property="twitter:domain" content="next-web-rose.vercel.app" />{" "}
+      {/* cambiar por chh.work en el futuro */}
       <meta property="twitter:description" content={description} />
       <meta property="twitter:author" content={author} />
       <meta property="twitter:image" content={image} />
-
       <link rel="icon" href={icon} />
-
+      {/* email */}
       <meta name="reply-to" content="cristian.huijse@gmail.com" />
       <link rev="made" href="mailto:cristian.huijse@gmail.com" />
     </Head>
