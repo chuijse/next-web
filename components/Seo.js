dea4263aa82a042a69c2b91ca81bc9d804b61a6b
chuijse <cristian.huijse@gmail.com>
2022-08-16 13:00:34 -0400

    fix url in seo

--- components/Seo.js
+++ components/Seo.js
@@ -11,9 +11,10 @@ export default function Seo({
   linkedin = "https://cl.linkedin.com/in/cristianhuijse",
   github = "https://github.com/chuijse",
   article = false,
+  domain = "next-web-rose.vercel.app",
+  keywords = "Interacción digital,diseño,ux,ui,electrónica,front-end,arduino,diseño Industrial",
 }) {
   const router = useRouter();
-  console.log(router);
   return (
     <Head>
       <title>{`CHH | ${pageTitle}`}</title>
@@ -24,16 +25,16 @@ export default function Seo({
       <meta name="author" content={author} />
       <meta name="linkedin" content={linkedin} />
       <meta name="github" content={github} />
-      <meta
-        name="keywords"
-        content="Interacción digital,diseño,ux,ui,electrónica,front-end,arduino,diseño Industrial"
-      />
+      <meta name="keywords" content={keywords} />
       <meta name="Revisit-after" content="1 days" />
       <meta name="robots" content="index, follow" />
       {/* Facebook Meta Tags */}
       <meta property="og:title" content={`CHH | ${pageTitle}`} key="title" />
       <meta property="og:type" content={article ? "article" : "webpage"} />
-      <meta property="og:url" content={router.route} />
+      <meta
+        property="og:url"
+        content={router.route === "/" ? domain : `${domain}${router.pathname}`}
+      />
       <meta property="og:image" content={image} />
       <meta property="og:description" content={description} key="description" />
       <meta property="og:site_name" content="Cristian Huijse Portfolio" />
@@ -41,8 +42,11 @@ export default function Seo({
       {/* Twitter Meta Tags */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:title" content={`CHH | ${pageTitle}`} />
-      <meta property="twitter:url" content={router.route} />
-      <meta property="twitter:domain" content="next-web-rose.vercel.app" />{" "}
+      <meta
+        property="twitter:url"
+        content={router.route === "/" ? domain : `${domain}${router.pathname}`}
+      />
+      <meta property="twitter:domain" content={domain} />{" "}
       {/* cambiar por chh.work en el futuro */}
       <meta property="twitter:description" content={description} />
       <meta property="twitter:author" content={author} />
