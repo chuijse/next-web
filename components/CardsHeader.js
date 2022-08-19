9248a6a3bd24a7955757b8b09e61de8674394cec
chuijse <cristian.huijse@gmail.com>
2022-08-18 20:16:13 -0400

    fix header classes names

--- components/CardsHeader.js
+++ components/CardsHeader.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function CardsHeader({ title, subTitle }) {
   return (
-    <section className="intro">
+    <section className="cards-header">
       <motion.h1
         initial={{ clipPath: "inset(0% 0% 100% 0%)", y: "100%" }}
         animate={{ clipPath: "inset(0% 0% 0% 0%)", y: "0" }}
@@ -11,13 +11,13 @@ export default function CardsHeader({ title, subTitle }) {
         {title}
       </motion.h1>
       <motion.div
-        className="teaching-line"
+        className="line"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       />
       <motion.div
-        className="subHeader"
+        className="sub-header"
         initial={{ clipPath: "inset(100% 0% 0% 0%)", y: "-100%" }}
         animate={{ clipPath: "inset(0% 0% 0% 0%)", y: "0" }}
         transition={{ duration: 0.5 }}
