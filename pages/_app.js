e197eff500114fbd5a17d8d9b9a5d3823b5e817c
chuijse <cristian.huijse@gmail.com>
2022-08-28 20:05:44 -0400

    add referencecs

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ import { motion, AnimatePresence } from "framer-motion";
 //https://codesandbox.io/s/framer-motion-animatepresence-react-router-dom-9dhyn?from-embed
 //https://codesandbox.io/s/animated-page-transitions-in-nextjs-yb71o5
 
+//como quiero mi index
+//https:codesandbox.io/s/framer-motion-parallax-i9gwuc?from-embed=&file=/src/App.tsx:459-469
+
 function MyApp({ Component, pageProps, router }) {
   //console.log(router);
   return (
